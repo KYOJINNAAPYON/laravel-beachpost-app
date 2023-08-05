5c419dfa7276c95998f36a24b912c88b0aada182
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-05 12:55:29 +0900

    tagはnullでも可

--- database/migrations/2023_07_24_232453_create_posts_table.php
+++ database/migrations/2023_07_24_232453_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->string('tag');
+            $table->string('tag')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
