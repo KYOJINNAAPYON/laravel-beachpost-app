2d19ba036114b79ca1b8e6be8551cd250802c692
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-19 12:33:18 +0900

    indexにて画像表示（作成途中）

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,7 +22,7 @@
             Place : {{ $post->category_name }}
             </div>
             <div class="col-12 mb-4 mt-2">
-              <img src="{{ asset( $post_image )}}" width="25%">
+              <img src="{{ asset('public/'.$post->file_name) }}" width="25%">
             </div>
             <div class="row justify-content-end">
               <div class="col-2"></div>
