09d56a7ee1fed8efff43ee695ee48b8c76f213f4
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-19 13:59:15 +0900

    セレクトボックス追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,14 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                            <select name="prefecture_id">
+                                @foreach ($prefectures as $prefecture)
+                                    <option value="{{ $prefecture->id }}" class="form-control">{{ $prefecture->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
