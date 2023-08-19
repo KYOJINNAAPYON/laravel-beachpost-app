bef407f14bb1a3d9368c917cbfd695fa94a73ee1
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-19 13:58:59 +0900

    prefectureをcreateするのは削除

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'prefecture' => [],
+            'prefecture' => ['required'],
         ]);
     }
 
@@ -70,10 +70,14 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
-
-        return Prefecture::create([
             'prefecture' => $data['prefecture'],
         ]);
     }
+
+    public function index()
+    {                           
+        $prefectures = Prefecture::all();
+ 
+        return view('', compact('prefectures'));
+   }
 }
