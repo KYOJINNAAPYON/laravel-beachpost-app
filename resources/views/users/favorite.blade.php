024770bdf225c5139d718714d70e02a5a6c7d84f
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-22 13:54:24 +0900

    お気に入り一覧ページのCSS等修正

--- resources/views/users/favorite.blade.php
+++ resources/views/users/favorite.blade.php
@@ -14,12 +14,12 @@
                     <div class="d-inline-flex">
                       <div class="col m-3">
                         <a href="{{route('posts.show', $fav->favoriteable_id)}}" class="w-70">
-                        <img src="{{ asset($fav->image)}}" width="80%" class="d-block mx-auto">
+                        <img src="{{ asset(App\Models\Post::find($fav->favoriteable_id)->image)}}" width="80%" class="d-block mx-auto">
                         </a>
                       </div>
                       <div class="col-8 m-3">
                         <div class="col"><h5 class="w-100">{{App\Models\Post::find($fav->favoriteable_id)->title}}</h5></div>
-                        <div class="col"><h6 class="w-100"><div class="div-pre">{{App\Models\Post::find($fav->favoriteable_id)->content}}</div></h6></div>
+                        <div class="col"><h6 class="w-100"><div class="div-pre w-75">{{App\Models\Post::find($fav->favoriteable_id)->content}}</div></h6></div>
                         <h6 class="w-100">{{App\Models\Post::find($fav->favoriteable_id)->category->name}}</h5>
                       </div>
                     </div>
