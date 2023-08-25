ab043341801752bac327d520130b0ed13d0a2f17
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-08-25 21:55:07 +0900

    お気に入り一覧ページ修正

--- resources/views/users/favorite.blade.php
+++ resources/views/users/favorite.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
- 
+
  @section('content')
  <table>
      <div class="container mt-4">
@@ -7,21 +7,19 @@
 
              @foreach ($favorites as $fav)
 
-            <div class="container">
-             <div class="row">
+              <div class="row">
                 <div class="post">
-                  <div class="col-md-10 mt-2">
+                  
                     <div class="d-inline-flex">
-                      <div class="col m-3">
-                        <a href="{{route('posts.show', $fav->favoriteable_id)}}" class="w-70">
-                        <img src="{{ asset(App\Models\Post::find($fav->favoriteable_id)->image)}}" width="80%" class="d-block mx-auto">
-                        </a>
-                      </div>
-                      <div class="col-8 m-3">
-                        <div class="col"><h5 class="w-100">{{App\Models\Post::find($fav->favoriteable_id)->title}}</h5></div>
-                        <div class="col"><h6 class="w-100"><div class="div-pre w-75">{{App\Models\Post::find($fav->favoriteable_id)->content}}</div></h6></div>
-                        <h6 class="w-100">{{App\Models\Post::find($fav->favoriteable_id)->category->name}}</h5>
-                      </div>
+                    <div class="col m-3">
+                     <a href="{{route('posts.show', $fav->favoriteable_id)}}" class="w-70">
+                      <img src="{{  asset(App\Models\Post::find($fav->favoriteable_id)->image) }}" width="80%" class="d-block mx-auto">
+                     </a>
+                     <div class="col m-5">
+                        <div class="col"><h5>{{App\Models\Post::find($fav->favoriteable_id)->title}}</h5></div>
+                        <div class="col div-pre w-75">{{App\Models\Post::find($fav->favoriteable_id)->content}}</div>
+                        <h6 class="col">{{App\Models\Post::find($fav->favoriteable_id)->category->name}}</h6>
+                     </div>
                     </div>
                   </div>
                   <div class="row justify-content-between">
@@ -30,11 +28,10 @@
                       → <a href="{{ route('posts.favorite', $fav->favoriteable_id) }}" class="">Unliked</a>
                     </div>
                   </div>
-                </div>
-               </div>
+               
               </div>
-            
+            </div>
              @endforeach
-     </div>
+    </div>
  </table>
  @endsection
